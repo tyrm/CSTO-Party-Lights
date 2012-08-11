80c33076b95b946ce53727ec92231e4fd64af866
Wolfy Meier <wolfy@wlfy.it>
2012-08-10 23:16:24 -0700

    Added Sparkle and White. Begun adding "options".
    Adjusted beginning pin definitions to constants so they can be used in
    variables.
    Added random seed to seed with the noise on an analog line.
    Added Sparkle Mode. (one of my favourites)
    -Sparkle has 2 options
    --likelyhood: which is how often a pixel will turn white.
    --speed: how fast the pixels fade to black.
    Added White
    -White has 1 option
    --It adjusts between a nice white and a wretched all channels on white.

--- dayshade/dayshade.ino
+++ dayshade/dayshade.ino
@@ -4,17 +4,17 @@
 
 //Assign Pins
   //Ditigal
-int modeInterupt = 2; //Interupt Pin
-int dataPin  = 4; //Light
-int clockPin = 5;
+const int modeInterupt = 2; //Interupt Pin
+const int dataPin  = 4; //Light
+const int clockPin = 5;
   //Analog
 //int spectrumLeft = 0; //Unused in this version
 //int spectrumRight = 1; //Unused in this version
-int knoba = 2;
-int knobb = 3;
+const int knoba = 2;
+const int knobb = 3;
 
 //Init Light Pixels
-int numPixels = 140;
+const int numPixels = 140;
 Adafruit_WS2801 dayshade = Adafruit_WS2801(numPixels, dataPin, clockPin, WS2801_GRB);
 
 //Init Global Variables
@@ -28,24 +28,31 @@ void setup() {
   dayshade.begin(); 
   dayshade.show(); // Update LED contents, to start they are all 'off'
   
-  
+  randomSeed(analogRead(random(8)));
 }
 
+// You may have noticed I'm using 0-1023 as my range, this is because this is the
+// range returned by analogRead(). Since the range is constant it makes sense for
+// the function to assuming the incoming range and map() it to usable ranges.
 void loop() {
   switch (mode) {
     case 0:
       makeFire();
       break;
     case 1:
+      sparkle(1023,512);
+      break;
+    case 2:
+      white(0);
       break;
   }
 }
 
 //************************************************ Programs *************************************************//
-/***************** Make Fire *****************\
+/***** Make Fire ******\
 | A faux fire effect.  |
-| analoga: 
-\*****************************************/
+| analoga:             |
+\**********************/
 void makeFire(){
   for(int i=0; i<numPixels; i++){
     dayshade.setPixelColor(i,Color(random(120)+135,random(40),0));
@@ -61,7 +68,42 @@ void makeFire(){
   dayshade.show();   // write all the pixels out
   delay(60);
 }
-
+/***************************** Sparkle ****************************\
+| Makes a random pixel white at random interals that slowly plays  |
+| analoga:                                                         |
+\******************************************************************/
+void sparkle(int likelyhood,int speedArg){
+  static byte drop[numPixels];
+  int theSpeed = map(speedArg,0,1023,1,10);
+  
+  // This range may seem random, but my favourite setting is at 10
+  // and I wanted 10 to be exatly in the middle.
+  if(random(map(likelyhood,0,1023,2,18)==1)){
+    drop[random(numPixels)]=255;
+  }
+  for(int i=0; i<numPixels; i++){
+    dayshade.setPixelColor(i,Color(drop[i],map(drop[i],0,255,0,175),map(drop[i],0,255,0,140)));
+    
+    if(drop[i]>theSpeed){
+      drop[i]-=theSpeed;
+    }else {
+      drop[i]=0;
+    };
+  }
+  dayshade.show();   // write all the pixels out
+  delay(10);
+}
+/****************** White ******************\
+| A full white fill for full illimunation.  |
+| analoga:                                  |
+\*******************************************/
+void white(int shade){
+  for(int i=0; i<numPixels; i++){
+    dayshade.setPixelColor(i,Color(255,map(shade,0,1023,175,255),map(shade,0,1023,140,255)));
+  }
+  dayshade.show();   // write all the pixels out
+  delay(500);
+}
 
 //************************************************ Functions ************************************************//
 
